a39b1937f9e1588177c8ca436aeb8e657bde93e8
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-12-29 20:13:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+// Set the first panel as active initially
+panels[0].classList.add('active');
+
+// Add event listener for each panel
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();  // Remove active class from all panels
+        panel.classList.add('active');  // Add active class to clicked panel
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');  // Remove active class from all panels
+    });
+}
